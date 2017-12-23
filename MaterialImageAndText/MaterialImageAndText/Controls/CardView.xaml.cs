b57f45332b810da67e75432c0c85f39497ec4339
Enis Necipoğlu <doctorman_18@hotmail.com>
2017-12-23 21:21:20 +0300

    Add project files.

---
+++ MaterialImageAndText/MaterialImageAndText/Controls/CardView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaterialImageAndText.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardView : Frame
+    {
+        public CardView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            boxCardColor.HeightRequest = boxCardColor.Width / 16 * 9;
+            imgCard.HeightRequest = imgCard.Width / 16 * 9;
+
+        }
+        
+        private void Card_Tapped(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Views.CardDetail());
+        }
+    }
+}
---
+++ MaterialImageAndText/MaterialImageAndText/Views/CardDetail.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaterialImageAndText.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CardDetail : ContentPage
+    {
+        public CardDetail()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            imgCard.HeightRequest = imgCard.Width * 2 / 3; // set the height of Image to 3:2 Ratio
+
+
+
+            //This keeps all margin as same value which given from XAML. Only changes Top Margin as Image height
+            grdContentArea.Margin = new Thickness(grdContentArea.Margin.Left, imgCard.Height, grdContentArea.Margin.Right, grdContentArea.Margin.Bottom);
+
+            //Set the content height as scrollable
+            grdContentArea.HeightRequest = grdContentArea.Height > this.Height ? grdContentArea.Height : this.Height;
+
+            //boxCardColor.HeightRequest = boxCardColor.Width / 16 * 9;
+        }
+
+        private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
+        {
+            imgCard.TranslateTo(0, -1 * (e.ScrollY / 2), 2);
+            if (e.ScrollY < imgCard.Height)
+            {
+                imgFloatButton.TranslateTo(0,e.ScrollY /5, 25);
+            }
+            else
+            {
+                imgFloatButton.TranslateTo(0, this.Height - imgFloatButton.Height, 100);
+            }
+
+        }
+    }
+}
